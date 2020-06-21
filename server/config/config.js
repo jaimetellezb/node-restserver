e12be097703ff5a34cedad09616b4f39c47ea8fc
jaimetellezb <jaimetellez410@gmail.com>
2020-06-20 22:29:41 -0500

    Google sign-In

--- server/config/config.js
+++ server/config/config.js
@@ -26,8 +26,14 @@ process.env.SEED = process.env.SEED || "este-es-el-seed-desarrollo";
 /**
  * cadena conexión MongoDB
  */
-
 process.env.URL_MONGODB =
     process.env.NODE_ENV === "dev" ?
     "mongodb://localhost:27017/cafe" :
     process.env.MONGO_URL;
+
+/**
+ * google client ID
+ */
+process.env.CLIENT_ID =
+    process.env.CLIENT_ID ||
+    "558451916625-10ngkg9ldnh8lh3v4g1kl7c63e3so4u9.apps.googleusercontent.com";
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ require("./config/config");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
+
 const app = express();
 
 // parse application/x-www-form-urlencoded
@@ -9,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// habilitar la carpeta public
+app.use(express.static(path.resolve("public")));
 
 //Configuración global de rutas
 app.use(require("./routers/index"));
